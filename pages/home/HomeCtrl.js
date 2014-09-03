2b850604150ce74bb27dfbc8df3c616e8e758932
Chris Esplin <christopher.esplin@gmail.com>
2014-09-02 22:02:19 -0600

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'quiverUtilities', function($scope, quiverUtilities) {
+	//TODO - put any directive code here
+}]);
